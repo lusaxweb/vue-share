c70a3bfd203b2d8109d4fc1684a5f9ea7aeac5fd
ldrovira <luisdanielrovira8@gmail.com>
2018-10-10 17:42:06 -0400

    deploy

---
+++ precache-manifest.eebac09e0f99b45a45fead56af3d1ca1.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "e79bfd88537def476913f3ed52f4f4b3",
+    "url": "/vue-share/fonts/MaterialIcons-Regular.e79bfd88.eot"
+  },
+  {
+    "revision": "a74de9545779113f249f",
+    "url": "/vue-share/css/about.d41b8381.css"
+  },
+  {
+    "revision": "4d361a7c9e674e8cfffe",
+    "url": "/vue-share/css/app.8803beda.css"
+  },
+  {
+    "revision": "4d361a7c9e674e8cfffe",
+    "url": "/vue-share/js/app.87585ab1.js"
+  },
+  {
+    "revision": "1bef7ab58c07a3760ead",
+    "url": "/vue-share/css/chunk-3f7d56af.a7bf79e3.css"
+  },
+  {
+    "revision": "1bef7ab58c07a3760ead",
+    "url": "/vue-share/js/chunk-3f7d56af.05c037d5.js"
+  },
+  {
+    "revision": "7c5f8361ff7a91aec9a5",
+    "url": "/vue-share/css/chunk-vendors.14ecfbe1.css"
+  },
+  {
+    "revision": "7c5f8361ff7a91aec9a5",
+    "url": "/vue-share/js/chunk-vendors.cd3ac684.js"
+  },
+  {
+    "revision": "570eb83859dc23dd0eec423a49e147fe",
+    "url": "/vue-share/fonts/MaterialIcons-Regular.570eb838.woff2"
+  },
+  {
+    "revision": "012cf6a10129e2275d79d6adac7f3b02",
+    "url": "/vue-share/fonts/MaterialIcons-Regular.012cf6a1.woff"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/vue-share/fonts/MaterialIcons-Regular.a37b0c01.ttf"
+  },
+  {
+    "revision": "a74de9545779113f249f",
+    "url": "/vue-share/js/about.436eb040.js"
+  },
+  {
+    "revision": "9c4a0ef0e03e723e8a8ce1dca9eb1f29",
+    "url": "/vue-share/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-share/robots.txt"
+  },
+  {
+    "revision": "90575f3c385acfef9092f78729c4437e",
+    "url": "/vue-share/apps/vueshare.png"
+  },
+  {
+    "revision": "05af76365a8444d1219895c09d7bfb73",
+    "url": "/vue-share/apps/vuesax.png"
+  },
+  {
+    "revision": "319a59f426abc4fbcb091bbeb03524a4",
+    "url": "/vue-share/apps/vuenut.png"
+  },
+  {
+    "revision": "728fe425dc15279f7c6cdae1f3f7088f",
+    "url": "/vue-share/apps/lusaxweb-blanco.png"
+  },
+  {
+    "revision": "09793ea1f42721894c687c4642098ae2",
+    "url": "/vue-share/apps/logo.png"
+  },
+  {
+    "revision": "6d28ed4542678b6c5e669c99126286bb",
+    "url": "/vue-share/apps/devawesome.png"
+  },
+  {
+    "revision": "808122c47a93aa25d3d388f99820341f",
+    "url": "/vue-share/apps/devawesome-blanco.png"
+  },
+  {
+    "revision": "c601bcdad6ad13edc34d2a60938145b4",
+    "url": "/vue-share/Screenshot_75.png"
+  }
+];
